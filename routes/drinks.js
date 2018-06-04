793ffa8c1d1a2761c1fbaba0dd3ac184be93eac9
Petrus Hoc <hoc081098@gmail.com>
2018-06-04 23:01:57 +0700

    Change /star

--- routes/drinks.js
+++ routes/drinks.js
@@ -58,72 +58,120 @@ router.post('/star', async (req, res) => {
     if (!phone || !drink_id) {
         return res.status(422).json({message: "Required phone and drink id"});
     }
-
     try {
         const countUser = await User.count({phone: phone});
         if (countUser === 0) {
             return res.status(404).json({message: `User with phone ${phone} not found`});
         }
 
-        const countDrink = await Drink.find({_id: drink_id}).limit(1);
-        if (countDrink.length === 0) {
+        const find = await Drink.find({_id: drink_id}).limit(1);
+        if (find.length === 0) {
             return res.status(404).json({message: `Drink with id ${drink_id} not found`});
         }
+        const drink = find[0];
+        if (drink.stars.includes(phone)) { //already have star => unstar
+            await Fawn.Task()
+                .update(User, {phone: phone}, {$pop: {staredDrinkIds: drink_id}})
+                .update(Drink, {_id: drink_id}, {$pop: {stars: phone}, $inc: {starCount: -1}})
+                .run({useMongoose: true});
 
-        const r = await Fawn.Task()
-            .update(User, {phone: phone}, {$push: {staredDrinkIds: drink_id}})
-            .update(Drink, {_id: drink_id}, {$push: {stars: phone}, $inc: {starCount: 1}})
-            .run({useMongoose: true});
+            return res.status(200).json({
+                ...drink.toObject(),
+                stars: (() => {
+                    const stars = drink.stars;
+                    let index = stars.indexOf(phone);
+                    return [...stars.slice(0, index), ...stars.slice(index + 1)];
+                })(),
+                starCount: drink.starCount - 1
+            });
+        } else {
+            await Fawn.Task()
+                .update(User, {phone: phone}, {$push: {staredDrinkIds: drink_id}})
+                .update(Drink, {_id: drink_id}, {$push: {stars: phone}, $inc: {starCount: 1}})
+                .run({useMongoose: true});
 
-
-        const drink = countDrink[0];
-        return res.status(200).json({
-            ...drink.toObject(),
-            stars: [...drink.stars, phone],
-            starCount: drink.starCount + 1
-        });
+            return res.status(200).json({
+                ...drink.toObject(),
+                stars: [...drink.stars, phone],
+                starCount: drink.starCount + 1
+            });
+        }
     } catch (e) {
         res.status(e.status || 500).json({message: e.message});
     }
 });
 
-
-router.delete('/star', async (req, res) => {
-    const {phone, drink_id} = req.body;
-    if (!phone || !drink_id) {
-        return res.status(422).json({message: "Required phone and drink id"});
-    }
-
-    try {
-        const countUser = await User.count({phone: phone});
-        if (countUser === 0) {
-            return res.status(404).json({message: `User with phone ${phone} not found`});
-        }
-
-        const countDrink = await Drink.find({_id: drink_id}).limit(1);
-        if (countDrink.length === 0) {
-            return res.status(404).json({message: `Drink with id ${drink_id} not found`});
-        }
-
-        const r = await Fawn.Task()
-            .update(User, {phone: phone}, {$pop: {staredDrinkIds: drink_id}})
-            .update(Drink, {_id: drink_id}, {$pop: {stars: phone}, $inc: {starCount: -1}})
-            .run({useMongoose: true});
-
-
-        const drink = countDrink[0];
-        return res.status(200).json({
-            ...drink.toObject(),
-            stars: (() => {
-                const stars = drink.stars;
-                let index = stars.indexOf(phone);
-                return [...stars.slice(0, index), ...stars.slice(index + 1)];
-            })(),
-            starCount: drink.starCount - 1
-        });
-    } catch (e) {
-        res.status(e.status || 500).json({message: e.message});
-    }
-});
+// router.post('/star', async (req, res) => {
+//     const {phone, drink_id} = req.body;
+//     if (!phone || !drink_id) {
+//         return res.status(422).json({message: "Required phone and drink id"});
+//     }
+//
+//     try {
+//         const countUser = await User.count({phone: phone});
+//         if (countUser === 0) {
+//             return res.status(404).json({message: `User with phone ${phone} not found`});
+//         }
+//
+//         const countDrink = await Drink.find({_id: drink_id}).limit(1);
+//         if (countDrink.length === 0) {
+//             return res.status(404).json({message: `Drink with id ${drink_id} not found`});
+//         }
+//
+//         const r = await Fawn.Task()
+//             .update(User, {phone: phone}, {$push: {staredDrinkIds: drink_id}})
+//             .update(Drink, {_id: drink_id}, {$push: {stars: phone}, $inc: {starCount: 1}})
+//             .run({useMongoose: true});
+//
+//
+//         const drink = countDrink[0];
+//         return res.status(200).json({
+//             ...drink.toObject(),
+//             stars: [...drink.stars, phone],
+//             starCount: drink.starCount + 1
+//         });
+//     } catch (e) {
+//         res.status(e.status || 500).json({message: e.message});
+//     }
+// });
+//
+//
+// router.delete('/star', async (req, res) => {
+//     const {phone, drink_id} = req.body;
+//     if (!phone || !drink_id) {
+//         return res.status(422).json({message: "Required phone and drink id"});
+//     }
+//
+//     try {
+//         const countUser = await User.count({phone: phone});
+//         if (countUser === 0) {
+//             return res.status(404).json({message: `User with phone ${phone} not found`});
+//         }
+//
+//         const countDrink = await Drink.find({_id: drink_id}).limit(1);
+//         if (countDrink.length === 0) {
+//             return res.status(404).json({message: `Drink with id ${drink_id} not found`});
+//         }
+//
+//         const r = await Fawn.Task()
+//             .update(User, {phone: phone}, {$pop: {staredDrinkIds: drink_id}})
+//             .update(Drink, {_id: drink_id}, {$pop: {stars: phone}, $inc: {starCount: -1}})
+//             .run({useMongoose: true});
+//
+//
+//         const drink = countDrink[0];
+//         return res.status(200).json({
+//             ...drink.toObject(),
+//             stars: (() => {
+//                 const stars = drink.stars;
+//                 let index = stars.indexOf(phone);
+//                 return [...stars.slice(0, index), ...stars.slice(index + 1)];
+//             })(),
+//             starCount: drink.starCount - 1
+//         });
+//     } catch (e) {
+//         res.status(e.status || 500).json({message: e.message});
+//     }
+// });
 
 module.exports = router;
